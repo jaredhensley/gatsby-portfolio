0af0b049de22cdc3b27aa17845f3e62d468049c0
jaredhensley <jahensle85@me.com>
2020-04-27 22:00:10 -0500

    contentful cms source configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,13 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     }
   ],
 }
